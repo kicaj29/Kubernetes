38fe42dac0f69894863032d9de273d2521bf4810
kicaj29 <kicaj29@wp.pl>
2021-08-19 13:43:50 +0200

    init version health-check-aspnet-core

---
+++ health-check-aspnet-core/health-check-aspnet-core/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace health_check_aspnet_core
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
